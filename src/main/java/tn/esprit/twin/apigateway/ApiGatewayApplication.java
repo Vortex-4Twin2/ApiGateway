f7b21aad154dc1383001c1ecf2fa2f48ec4d2d84
souhirkrizi <Souhir.Krizi@esprit.tn>
2026-02-14 15:06:15 +0100

    Initial commit - Spring Cloud Gateway setup

---
+++ src/main/java/tn/esprit/twin/apigateway/ApiGatewayApplication.java
@@ -0,0 +1,26 @@
+package tn.esprit.twin.apigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class ApiGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiGatewayApplication.class, args);
+    }
+
+    @Bean
+    public RouteLocator getRoutes(RouteLocatorBuilder builder) {
+        return builder.routes().route(
+                        "Produits", r -> r.path("/products/**").uri("lb://MicroserviceProduits"))
+                .build();
+    }
+
+}
